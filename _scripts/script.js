4d0af9467ad9b9e968747df96ce00e20ccc7bd33
Renato <renatojunior7@hotmail.com>
2024-03-12 22:21:28 -0300

    continuacao do if-else

--- _scripts/script.js
+++ _scripts/script.js
@@ -130,16 +130,16 @@ function fruit() {
 
     if (appleQtd > 5 || strawberryQtd > 5) {
         total = parseFloat(appleQtd * 1.5) + parseFloat(strawberryQtd * 2.2);
-     
+
     }
-  
- 
+
+
     else {
         total = parseFloat(appleQtd * 1.8) + parseFloat(strawberryQtd * 2.5);
     }
 
     if (qtdKg > 8 || total > 25) {
-        total = total - (total * 10/100);
+        total = total - (total * 10 / 100);
     }
 
 
@@ -148,5 +148,146 @@ function fruit() {
     alert(total);
 
 }
+/* fruit(); */
 
-fruit();
+
+function school() {
+    let mediaFinal;
+
+    mediaFinal = Number(prompt('digite a média: '));
+
+    if (mediaFinal < 5) {
+        alert('Conceito D');
+    }
+    else if (mediaFinal >= 5 && mediaFinal < 7) {
+        alert('Conceito C');
+    }
+    else if (mediaFinal >= 7 && mediaFinal < 9) {
+        alert('Conceito B')
+    }
+    else if (mediaFinal >= 9 && mediaFinal <= 10) {
+        alert('Conceito A');
+    }
+
+}
+
+function imc() {
+    let peso, altura, imc;
+    peso = Number(prompt('Qual o seu peso?'));
+    altura = Number(prompt('Qual a sua altura?'));
+
+    imc = peso / (altura * altura);
+
+    if (imc < 18.5) {
+        alert(imc.toFixed(2) + ' - Abaixo do peso');
+    }
+    else if (imc >= 18.5 && imc < 25) {
+        alert(imc.toFixed(2) + ' - Peso normal');
+    }
+    else if (imc >= 25 && imc < 30) {
+        alert(imc.toFixed(2) + ' - Acima do peso');
+    }
+    else {
+        alert(imc.toFixed(2) + ' - Obeso');
+    }
+}
+/* imc(); */
+
+function test() {
+    let tabuada, count;
+
+    tabuada = Number(prompt('Qual tabuada?'));
+
+    for (count = 1; count <= 10; count++) {
+        console.log(tabuada + ' x ' + count + ' = ' + (tabuada * count));
+    }
+}
+
+/* test(); */
+
+function impar() {
+
+    let num;
+
+    num = Number(prompt('digite um valor'));
+
+    for (let count = 0; count < num; count++) {
+        if (count % 2 != 0) {
+            console.log(count);
+        }
+    }
+}
+/* impar(); */
+
+function sub() {
+    let qtd, x, y;
+    qtd = Number(prompt('quantas contas? '));
+
+    for (let count = 1; count <= qtd; count++) {
+        alert('Conta: ' + count)
+        x = Number(prompt('primeiro valor: '));
+        y = Number(prompt('segundo valor: '));
+
+        alert('Resultado: ' + x + ' - ' + y + ' = '+ (x-y));
+    }
+}
+/* 
+sub(); */
+
+function rent(){
+    let tipoDeCarro, qtdDias, qtdKm, total;
+
+    tipoDeCarro = prompt('Qual o tipo de carro? ');
+
+    qtdDias = Number(prompt('Quantos dias de aluguel'));
+
+    qtdKm = Number(prompt('Quantos kilometros percorridos'));
+
+    if(tipoDeCarro == 'popular'){
+        if(qtdKm <= 100){
+            total = (qtdDias * 90) + (qtdKm * 0.2); 
+        }
+        else if (qtdKm > 100){
+            total = (qtdDias * 90) + (qtdKm * 0.1);
+        }
+    }
+    else if (tipoDeCarro == 'luxo'){
+        if (qtdKm <= 200){
+            total = (qtdDias * 150) + (qtdKm * 0.3);
+        }
+        else if(qtdKm > 200){
+            total = (qtdDias * 150) + (qtdKm * 0.25);
+        }
+    }
+
+    alert('tipo: '+tipoDeCarro + ' dias: '+ qtdDias + ' Km: ' + qtdKm + ' total: ' + total.toFixed(2));
+}
+
+/* rent();
+ */
+
+function gym(){
+    let hr, pts, total;
+
+    hr = Number(prompt('horas: '));
+    
+    if(hr <= 10){
+        pts = hr * 2;
+        
+    }
+    else if(hr > 10 && hr <=20){
+        pts = hr * 5;
+    }
+
+    else if(hr > 20){
+        pts = hr * 10;
+    }
+
+
+
+    total = pts * 0.05;
+
+    alert(total.toFixed(2));
+}
+
+gym();
